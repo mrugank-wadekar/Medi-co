59f3beb9a30a1adfd9e13937ed2e8a97e4cf3ba9
mrugank-wadekar <mrugank9009@gmail.com>
2023-04-07 20:24:48 +0530

    Specialist Responsive UI changes

--- main.js
+++ main.js
@@ -36,6 +36,55 @@ if (window.innerWidth < 1030 || 'ontouchstart' in window) {
   console.log(temp,temp2);
 }
 
+  //Mobile hover effects for specialist section
+  if(window.innerWidth <610){
+  const mob2 = document.getElementById('mob-2');
+  const mob1 = document.getElementById('mob-1');
+  const mob3 = document.getElementById('mob-3');
+  const mob4 = document.getElementById('mob-4');
+
+  let ogmob1= mob1.style.transform;
+  let ogmob2= mob2.style.transform;
+  let ogmob3= mob3.style.transform;
+  let ogmob4= mob4.style.transform;
+  
+  mob2.addEventListener('mouseover', () => {
+    mob1.style.transform = 'translateY(-1.5rem)';
+  });
+  
+  mob2.addEventListener('mouseout', () => {
+    mob1.style.transform = ogmob1;
+  });
+
+  mob2.addEventListener('mouseover', () => {
+    mob3.style.transform = 'translateY(1.5rem)';
+  });
+  
+  mob2.addEventListener('mouseout', () => {
+    mob3.style.transform = ogmob3;
+  });
+
+  mob3.addEventListener('mouseover', () => {
+    mob4.style.transform = 'translateY(1.5rem)';
+  });
+  
+  mob3.addEventListener('mouseout', () => {
+    mob4.style.transform = ogmob4;
+  });
+
+  mob3.addEventListener('mouseover', () => {
+    mob2.style.transform = 'translateY(-1.5rem)';
+  });
+  
+  mob3.addEventListener('mouseout', () => {
+    mob2.style.transform = ogmob2;
+  });
+
+
+
+}
+  
+
 //change navbar color on scroll
 
 window.addEventListener("scroll",()=>{
@@ -73,6 +122,8 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
   });
+
+
 
 
   
